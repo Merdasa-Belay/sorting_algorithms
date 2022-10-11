b5816bb7e30407f86efc37d289c25ffa8e9fa197
Merdasa-Belay <merdasabelay@gmail.com>
2022-10-11 11:26:12 +0300

    Error Fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,26 +1,27 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Calls function
- * @array: Array to be sorted
- * @size: Size of array
- * Description: Function that sorts an array using the bubble sort method
- * Return: 0
- */
+  * bubble_sort - A function for bubble sorting.
+  * @array: The array to sort.
+  * @size: THe length of the aaray.
+  * Return: Nothing.
+  */
 void bubble_sort(int *array, size_t size)
 {
-unsigned int i, j;
+size_t i = 0, j = 0;
+int aux = 0;
 
-if (!array)
+if (array == NULL || size == 0)
 return;
-
-for (i = 0; i < size - 1; i++)
+for (; i < size - 1; i++)
 {
 for (j = 0; j < size - i - 1; j++)
 {
 if (array[j] > array[j + 1])
 {
-swap(&array[j], &array[j + 1]);
+aux = array[j + 1];
+array[j + 1] = array[j];
+array[j] = aux;
 print_array(array, size);
 }
 }
